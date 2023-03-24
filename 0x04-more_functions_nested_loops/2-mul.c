346a27602c4a5e544dde6fa8dcc9dc9d4ac25437
Nacereddine <majidnacereddine@gmail.com>
2023-03-24 07:06:48 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Prints the digit
+ *
+ * @a: input number as an integer.
+ * @b: input number as an integer.
+ *
+ * Return: last digit.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
