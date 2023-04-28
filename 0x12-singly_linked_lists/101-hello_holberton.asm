b8e45ee7fdaadf3bca51b14e5013cdf974084aeb
Nacereddine <majidnacereddine@gmail.com>
2023-04-28 07:03:36 -0700

    hello-holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,22 @@
 section .data
-    hello db 'Hello, Holberton!', 0Ah ; the string to print
+    message db "Hello, Holberton", 0
+    format db "%s\n", 0
 
 section .text
-    global _start ; the entry point for the program
+    global main
 
-_start:
-    ; write the string to stdout
-    mov eax, 4 ; the system call for write
-    mov ebx, 1 ; file descriptor for stdout
-    mov ecx, hello ; the string to print
-    mov edx, 16 ; the length of the string
-    int 0x80 ; call the kernel
+main:
+    push rbp
+    mov rbp, rsp
 
-    ; exit the program
-    mov eax, 1 ; the system call for exit
-    xor ebx, ebx ; the exit code (0)
-    int 0x80 ; call the kernel
+    sub rsp, 8
+    lea rdi, [message]
+    lea rsi, [format]
+    mov al, 0
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    xor eax, eax
+    ret
 
