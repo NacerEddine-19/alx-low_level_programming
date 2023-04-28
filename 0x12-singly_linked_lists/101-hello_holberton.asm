1c410a6a7c5efa2798f3ddffbbcf38b2d4bd7d9e
Nacereddine <majidnacereddine@gmail.com>
2023-04-28 09:02:36 -0700

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,14 @@
 section .data
-    message db "Hello, Holberton", 0
-    format db "%s\n", 0
-
+    message db "Hello, Holberton", 10, 0
 section .text
     global main
-
+    extern printf
 main:
     push rbp
     mov rbp, rsp
-
-    sub rsp, 8
-    lea rdi, [message]
-    lea rsi, [format]
+    mov rdi, message
     mov al, 0
     call printf
-
     mov rsp, rbp
     pop rbp
-    xor eax, eax
     ret
-
